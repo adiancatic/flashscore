ca0ddd88b402327c747930a06b3670954f3199ff
adian <adiancatic@gmail.com>
2020-12-13 16:13:59 +0100

    ADD: Added bottom navigation

--- app/src/main/java/com/example/flashscore/MainActivity.java
+++ app/src/main/java/com/example/flashscore/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.flashscore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.flashscore.Api.FootballApi;
 import com.example.flashscore.DataModels.Country;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +36,20 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(countryList);
             }
         });
+
+        /*
+         * Navigation
+         */
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        Set<Integer> topLevelDestinations = new HashSet<>();
+        topLevelDestinations.add(R.id.fr_matches_all);
+        topLevelDestinations.add(R.id.fr_matches_live);
+        topLevelDestinations.add(R.id.fr_more_actions);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(topLevelDestinations).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     }
 
 }
