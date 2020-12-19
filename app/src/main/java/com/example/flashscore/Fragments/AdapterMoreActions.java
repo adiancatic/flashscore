6a13a5abe3d12b43a4f9980ae9c4a220fb2c76a7
adian <adiancatic@gmail.com>
2020-12-19 22:01:37 +0100

    ADD: Added MoreActions recycler view

---
+++ app/src/main/java/com/example/flashscore/Fragments/AdapterMoreActions.java
@@ -0,0 +1,55 @@
+package com.example.flashscore.Fragments;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.flashscore.MoreActionsItem;
+import com.example.flashscore.R;
+
+import java.util.List;
+
+public class AdapterMoreActions extends RecyclerView.Adapter<AdapterMoreActions.ViewHolder> {
+
+    List<MoreActionsItem> moreActionsItemList;
+
+    public AdapterMoreActions(List<MoreActionsItem> moreActionsItemList) {
+        this.moreActionsItemList = moreActionsItemList;
+    }
+
+    @NonNull
+    @Override
+    public AdapterMoreActions.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.frame_more_actions_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterMoreActions.ViewHolder holder, int position) {
+        holder.actionIcon.setImageResource(moreActionsItemList.get(position).getIcon());
+        holder.actionName.setText(moreActionsItemList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return moreActionsItemList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView actionIcon;
+        TextView actionName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            actionIcon = itemView.findViewById(R.id.iv_more_actions_item_icon);
+            actionName = itemView.findViewById(R.id.tv_more_actions_item_name);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/flashscore/Fragments/FragmentMoreActions.java
@@ -0,0 +1,52 @@
+package com.example.flashscore.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.flashscore.MoreActionsItem;
+import com.example.flashscore.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentMoreActions extends Fragment {
+
+    private RecyclerView recyclerView;
+    private List<MoreActionsItem> moreActionsItemList;
+
+    public FragmentMoreActions() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_more_actions, container, false);
+
+        recyclerView = view.findViewById(R.id.rv_more_actions_list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        recyclerView.setAdapter(new AdapterMoreActions(initData()));
+
+        return view;
+    }
+
+    private List<MoreActionsItem> initData() {
+        moreActionsItemList = new ArrayList<>();
+        moreActionsItemList.add(new MoreActionsItem(R.drawable.ic_football_ball, "Option 1"));
+        moreActionsItemList.add(new MoreActionsItem(R.drawable.ic_football_ball, "Option 2"));
+        moreActionsItemList.add(new MoreActionsItem(R.drawable.ic_football_ball, "Option 3"));
+        return moreActionsItemList;
+    }
+
+}
